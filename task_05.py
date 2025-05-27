156ceb46cef459471f16b144617a2c160287cf20
Bogdan <bgdnvlkvb7@gmail.com>
2025-05-27 20:57:26 +0300

    mistakes were made in task 5

--- task_05.py
+++ task_05.py
@@ -4,13 +4,8 @@ def date_in_future(integer):
 
     if isinstance(integer, int):
         required_date = datetime.now() + timedelta(days=integer)
-        formatted_date = required_date.strftime("%d-%m-%Y %H:%M:%S")
-        print(formatted_date)
     else:
-        current_date = datetime.now()
-        print(current_date)
-
-date_in_future(365)
-
-#в условии написано то вернуть дату, то вывести дату
-#в примере метод вызван без print, так что решил именно выводить дату, вотт
+        required_date = datetime.now()
+     
+    formatted_date = required_date.strftime("%d-%m-%Y %H:%M:%S")
+    return formatted_date
