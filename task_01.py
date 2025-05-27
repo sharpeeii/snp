09058f58f866e99e0f94b207678d4df9626229b1
Bogdan <bgdnvlkvb7@gmail.com>
2025-05-27 20:54:21 +0300

    mistakes were made

--- task_01.py
+++ task_01.py
@@ -1,8 +1,11 @@
-def format_string(string:str):
+def format_string(string):
     result = ''.join(ch.lower() for ch in string if ch.isalnum())
     return result
 
-def is_palindrome(string: str):
-    formatted = format_string(string)
+def is_palindrome(string):
+    if string is None:
+        return False
+    formatted = format_string(str(string))
     return formatted == formatted[::-1]
 
+print(is_palindrome(None))
